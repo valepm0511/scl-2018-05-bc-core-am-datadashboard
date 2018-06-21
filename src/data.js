6d18847bd3e7efa5e91b5da74117f7d35953304a
Paulifer <pauli.hua.timm@gmail.com>
2018-06-21 13:07:31 -0400

    tratando de que me de la informacion de los cochorts

--- src/data.js
+++ src/data.js
@@ -1,48 +1,56 @@
 
+var users = null;
+var progress = null;
+var cohorts = null;
 
-const usersJson= '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-const progressJson = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-const cohortsJson = '../data/cohorts/cohorts.json';
-
-fetch(usersJson)
+const usersData= '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+const progressData = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+const cohortsData= '../data/cohorts.json';
+//para conseguir el nombre de las alumnas
+fetch(usersData)
 .then(response => response.json())
-.then(users => {
+.then(data => {
+	users = data;
 	listStudents(users);
+	idsStudents(users);
 })
 const listStudents = users=>{
 	let NewList = [];
 	users.forEach(e =>{
-		
-			NewList.push(e.name);
-			console.log(NewList);
-		
+		NewList.push(e.name);
+			//console.log(NewList);
+		});
+}
+const idsStudents = users=>{
+	let ListIds= [];
+	users.forEach(e =>{
+		ListIds.push(e.id);
+		//console.log(ListIds);
 	});
-
-	
 }
 
-//guardando en un arreglo y pushando
-const nameUsers = (users) =>{
-	let usersArray = [];
-	users.forEach(element => {
-		usersArray.push(element.name);
-	});
-	console.log(usersArray);
-}
-
-var progressData = fetch(progressJson)
+ fetch(progressData)
 .then(response => response.json())
-.then(progressJson => {
-	const progress = Object.entries(progressJson);
-	progressData = progress;
+.then(progreso => {
+	progress = progreso;
 	recolector();
+	//console.log(progress);
+	//listProgress(progreso);
 });
 
+/*const listProgress = (progreso)=>{
+	let newListProgress = [];
+	progreso.forEach(e =>{
+		newListProgress.push(e.units);
+		console.log(newListProgress);
+	})
+}*/
 
-var cohortsData = fetch(cohortsJson)
+
+ fetch(cohortsData)
 .then(response => response.json())
 .then(courses => {
-	cohortsData = courses;
+	cohorts = courses;
 	recolector();
 	nameCourse(courses);
 });
@@ -51,17 +59,31 @@ const nameCourse = (courses) =>{
 	let coursesArray = [];
 	courses.forEach(element => {
 		coursesArray.push(element.coursesIndex);
+//		console.log(coursesArray);
 	});
-	console.log(coursesArray);
+
+
+	//console.log(coursesArray);
 }
+ /*function recorrer(userData , progressData){
+ 	for (let i = 0; i < ListIds.length; i++) {
+ 		for (let j = 0; j < progress.length; j++) {
+ 			if(progress[j] == ListIds[i]){
 
-
+ 			}
+ 		}
+ 	}
+ 	console.log(recorrer);
+ }
+*/
 
 function recolector(){
-	if(usersData && progressData && cohortsData){
-		computeUsersStats(usersData, progressData, ["curso"]);
+	if(users && progress && cohorts){
+		computeUsersStats(usersData, progressData, ["id", "start", "coursesIndex", "end", "userCount"]);
 	}
+	return recolector;
 }
+console.log(recolector);
 
 //Recorriendo objetos de progress
 
