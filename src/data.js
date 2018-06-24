e9657bf989a43c4c0cc6b784728f7af6aafea139
valeria <valeria.penaloza5@gmail.com>
2018-06-23 21:13:29 -0400

    haciendo todo de nuevo

--- src/data.js
+++ src/data.js
@@ -1,66 +1,7 @@
-//guardando en variables las url
-const usersJson = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-const progressJson = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-const cohortsJson = '../data/cohorts.json';
-//guardando promesa usuario
-let usersData = fetch(usersJson)
-.then(response => response.json())
-.then(users => {
-	usersData = users;
-	
-	nameUsers(users);
-});
-//guardando en un arreglo y pushando de usuario
-const nameUsers = (users) =>{
-	let usersArray = [];
-	users.forEach(element => {
-		usersArray.push(element.name);
-	});
-	document.write(usersArray);
-}
-//guardando promesa progress
-let progressData = fetch(progressJson)
-.then(response => response.json())
-.then(progressJson => {
-	const progress = Object.entries(progressJson);
-	progressData = progress;
-	//console.log (progressData);
-	
-});
-
-for(var i in progressData){
-	//imprime todo el arreglo
-	console.log(progressData[i]['intro']['units']);
-//recorre intro
-console.log(Object.keys(progressData[i]['intro']['units']))
-
-//recorre todas las unidades
-for(var x in progressData[i]['intro']){
-	console.log(x);
-}
-}
-
-//guardando promesa cursos
-let cohortsData = fetch(cohortsJson)
-.then(response => response.json())
-.then(courses => {
-	cohortsData = courses;
-	nameCourse(courses);
-});
-//guardando en un arreglo y pushando de curso
-const nameCourse = (courses) =>{
-	let coursesArray = [];
-	courses.forEach(element => {
-		coursesArray.push(element.coursesIndex);
-	});
-	//console.log(coursesArray);
-}
-
-
-
 window.computeUsersStats = (users, progress, courses) =>{
-
-	
+	console.log(users);
+	console.log(progress);
+	console.log(courses);
 }
 window.sortUsers = (users, orderBy, orderDirection) =>{
 }
--- src/main.js
+++ src/main.js
@@ -14,6 +14,41 @@ window.onload = () => {
 		}
 	});
 }
+//guardando ruta de archivos user, progress y cohorts
+const usersJson = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+const progressJson = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+const cohortsJson = '../data/cohorts.json';
+	//conexion archivo user.jason
+	let usersData = fetch(usersJson)
+	.then(response => response.json())
+	.then(users => {
+		const usersArray = [];
+		usersData = users;
+		users.forEach(element => {
+			usersArray.push(element.name);
+		});
+		//conexion archivo progress.jason
+		let progressData = fetch(progressJson)
+		.then(response => response.json())
+		.then(progressJson => {
+			const progress = Object.entries(progressJson);
+			progressData = progress;
+			//conexion archivo cohorts.jason
+			let cohortsData = fetch(cohortsJson)
+			.then(response => response.json())
+			.then(courses => {
+				let coursesArray = [];
+				courses.forEach(element => {
+					coursesArray.push(element.coursesIndex);
+				});
+
+				window.computeUsersStats(usersArray,progressData,coursesArray);
+			});
+		});
+
+	});
+	//window.computeUsersStats(nameUsers(), "a", "a");
+
 
 
 
